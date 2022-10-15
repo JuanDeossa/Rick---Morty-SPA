643cd53f7607112d55db8a5eeafd5a9146c2883b
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-15 17:47:04 -0500

    routes/index.js

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import header from '@templates/header';
+import character from '@pages/character';
+import error404 from '@pages/error404';
+import home from '@pages/home';
+
+const routes={
+    "/":home,
+    "/:id":character,
+    "/contact":"contact"
+}
+
+const router= async ()=>{
+    const headerSelector = document.getElementById("header") //null pending
+    const contentSelector = document.getElementById("content") //null pending
+    headerSelector.innerHTML= await header()
+}
+
+export default router;
