b895626bceb785c38cd2dfbe5242dbf483bba6ba
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-15 18:31:01 -0500

    getHash

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ import character from '@pages/character';
 import error404 from '@pages/error404';
 import home from '@pages/home';
 
+//
+import getHash from '@utils/getHash';
+//
+
 const routes={
     "/":home,
     "/:id":character,
@@ -13,6 +17,7 @@ const router= async ()=>{
     const headerSelector = document.getElementById("header") //null pending
     const contentSelector = document.getElementById("content") //null pending
     headerSelector.innerHTML= await header()
+    console.log(getHash())
 }
 
 export default router;
