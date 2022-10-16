24c4ff62a7aee2c45922c86a0c75b3eaf3b664d8
JuanDeossa <juandeossa.nc@gmail.com>
2022-10-15 20:58:14 -0500

    getData function

---
+++ src/utils/getData.js
@@ -0,0 +1,12 @@
+const getData=async()=>{
+    const API = "https://rickandmortyapi.com/api/character"
+    try {
+        const response = await fetch(API)
+        const data = await response.json()
+        return data.results
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default getData
